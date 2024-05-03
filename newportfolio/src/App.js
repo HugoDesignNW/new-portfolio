c832c27e7ec5c9b7cf249caac448090eeb81d367
HugoDesignNW <hduranfe@sfu.ca>
2024-05-03 14:29:11 -0700

    Update App.js

--- newportfolio/src/App.js
+++ newportfolio/src/App.js
@@ -5,19 +5,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD <code>wenor</code> its beuatiful
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+
       </header>
+
+
+
+
+<div>
+  Hello World
+
+
+<div>
+
+
+<div className="flex">
+      <div className="flex-1 bg-gray-200 m-2 p-4">Div 1</div>
+      <div className="flex-1 bg-gray-200 m-2 p-4">Div 2</div>
+      <div className="flex-1 bg-gray-200 m-2 p-4">Div 3</div>
+      <div className="flex-1 bg-gray-200 m-2 p-4">Div 4</div>
+      <div className="flex-1 bg-gray-200 m-2 p-4">Div 5</div>
+    </div>
+
+
+
+</div>
+
+</div>
+
+
+
+
     </div>
   );
 }
